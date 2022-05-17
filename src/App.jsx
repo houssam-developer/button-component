982e5064ad096885cb7a5f773d2b0f28c673b5ef
houssam-dev <developpeur.pro.ma@gmail.com>
2022-05-17 06:27:25 +0100

    refactor: add disabled flag

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,29 @@ function App() {
 
 	return (
 		<div className="container">
-			<Button text="Default" />
+			<h1>Buttons</h1>
+
+			<div className='container-buttons'>
+				<h2>Default</h2>
+				<Button text="Default" />
+			</div>
+			<div className='container-buttons'>
+				<h2>Outline</h2>
+				<Button text="Default" variant='outline' />
+			</div>
+			<div className='container-buttons'>
+				<h2>Text</h2>
+				<Button text="Default" variant='text' />
+			</div>
+			<div className='container-buttons'>
+				<h2>Disable Shadow</h2>
+				<Button text="Default" variant='primary' disableShadow />
+			</div>
+
+			<div className='container-buttons'>
+				<h2>Disabled Button</h2>
+				<Button text="Default" disabled />
+			</div>
 		</div>
 	)
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
 
-function Button({ text, variant = 'text' }) {
+function Button({ text, variant = 'default', disableShadow, disabled }) {
 	// TODO guard clause in case of incorrect variant
 
 
+	console.log(`#text: ${text} #variant: ${variant} #disableShadow: ${disableShadow} #disabled: ${disabled} `);
+
+	if (variant === '') { variant = 'default'; }
+	let setClassNames = `btn btn--${variant}`;
+
+	if (disableShadow) { setClassNames = `${setClassNames} btn--disableshadow` }
+
+	function handleBtnClickEvent(e) {
+		console.log('click');
+	}
+
 	return (
-		<button className={`btn btn--${variant}`}>{text}</button>
+		<button className={setClassNames} disabled={disabled} onClick={handleBtnClickEvent}>{text}</button>
 	)
 }
 
